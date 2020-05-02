b67a6bf57bdbfacadc5263604120d2348adf308a
student <edd1e234@gmail.com>
2020-05-02 14:57:05 -0600

    Made some progress

---
+++ button_demo/.#switches.h
@@ -0,0 +1,1 @@
+student@systems-vm.7744:1588348989
---
+++ button_demo/stateMachinesVisual.h
@@ -0,0 +1,10 @@
+#ifndef stateMachinesVisual_included
+#define stateMachinesVisual_included
+
+void state_1_visual();
+void state_2_visual();
+void state_3_visual();
+void state_4_visual();
+
+#endif 
+
